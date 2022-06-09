785542e5ea90d071996f1bbd02693ae13ac83a4a
Joao Felipe Pimentel <joaofelipenp@gmail.com>
2022-06-08 23:10:29 -0700

    add classification state

---
+++ anachat/core/states/classification.py
@@ -0,0 +1,21 @@
+"""Classification helpers"""
+from ..handlers.action import ActionHandler
+from .utils import create_state_loader
+
+
+def classification_steps_state(comm, class_state=None):
+    """Shows classification steps"""
+    if class_state:
+        comm.memory["class_state"] = class_state
+    comm.memory["sub_state"] = "Classification"
+    comm.reply("Sounds good. Here are the steps for a classification:")
+    ActionHandler().show_options(comm, [
+        {'key': '1', 'label': 'Preprocessing', 
+         'state': create_state_loader('!subject Classification > Preprocessing')},
+        {'key': '2', 'label': 'Algorithm Specification',
+         'state': create_state_loader('!subject Classification > Algorithm Specification')},
+        {'key': '3', 'label': 'Validation',
+         'state': create_state_loader('!subject Classification > Validation')},
+        {'key': '4', 'label': 'Feature Engineering',
+         'state': create_state_loader('!subject Classification > Feature Engineering')},
+    ])
